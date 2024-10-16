54a7143efbc5a0e735bdd5058adab8e885b20315
Orlando Betancourth <orlando@uledger.io>
2024-10-15 19:33:41 -0600

    Configurando Vite Config como un MPA con glob patterns

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import * as glob from "glob";
+import path, { resolve } from "node:path";
+
+const getHtmlEntries = ()=>{
+    return Object.fromEntries(
+        [
+            ...glob.sync('./**/*.html', { ignore:'./dist/**'}).map(file=>[
+                file.slice(0, file.length - path.extname(file).length),
+                resolve(__dirname, file)
+            ])
+        ]
+    )
+}
+/*
+{
+    nombreArchiv: rutaDeArchivo,
+    nombreArchiv2: rutaDeArchivo2,
+}
+*/
+
+export default defineConfig(
+    {
+        appType: 'mpa',
+        base: "/",
+        build: {
+            rollupOptions: {
+                input: getHtmlEntries()
+            }
+        }
+    }
+);
